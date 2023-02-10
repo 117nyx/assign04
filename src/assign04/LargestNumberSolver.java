965babe76eed8a47b2a45b36de638e5e26547c91
JonathanK3 <u1400888@utah.edu>
2023-02-09 17:42:25 -0700

    added comparator

--- src/assign04/LargestNumberSolver.java
+++ src/assign04/LargestNumberSolver.java
@@ -238,6 +238,15 @@ public class LargestNumberSolver <T> {
             return 0;
         }
     }
+    protected static class KComparator implements Comparator {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            if (o1 instanceof Comparable<?> && o2 instanceof Comparable<?>)
+                return ((Comparable) o1).compareTo(o2);
+            return 0;
+        }
+    }
 }
 
 
