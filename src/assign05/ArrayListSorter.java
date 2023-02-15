3d50caca81e76723c92dc4aa1c819c725e541af1
Eden Harvey <edensharvey111@gmail.com>
2023-02-14 18:17:11 -0700

    Logic done for mergesort, need to implement driver methods

--- src/assign05/ArrayListSorter.java
+++ src/assign05/ArrayListSorter.java
@@ -11,15 +11,25 @@ import java.util.List;
 public class ArrayListSorter {
 public static int sizeToSwitch = 4;
     public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> arr){
+        //init arraylist of arraylists
         ArrayList<ArrayList<T>> mergeLists = new ArrayList<>();
         mergeLists.add(arr);
+        //divide until subarrays reach desired size
         while(mergeLists.get(0).size()>sizeToSwitch){
             mergeLists = mergeSplit(mergeLists);
         }
+        //sort small sub arrays
         for(ArrayList<T> obj: mergeLists){
             InsertionSort.sort(obj);
         }
 
+        while(mergeLists.size()!=1){
+            ArrayList<ArrayList<T>> temp = new ArrayList<>();
+            for(int i=0;i<mergeLists.size()-1;i+=2)
+                temp.add(merge(mergeLists.get(i),mergeLists.get(i+1)));
+            mergeLists = temp;
+        }
+        arr=mergeLists.get(0);
     }
 
     public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> arr){
@@ -47,13 +57,44 @@ public static int sizeToSwitch = 4;
             arr.add(i);
         return arr;
     }
-    private static <T> ArrayList<ArrayList<T>> mergeSplit(ArrayList<ArrayList<T>> arr){
+    private static <T extends Comparable<?super T>> ArrayList<ArrayList<T>> mergeSplit(ArrayList<ArrayList<T>> arr){
         ArrayList<ArrayList<T>> outList= new ArrayList<>();
         for(int i = 0;i<arr.size();i++){
             outList.add(new ArrayList<>(arr.get(i).subList(0,arr.get(i).size()/2)));
             outList.add(new ArrayList<>(arr.get(i).subList(arr.get(i).size()/2,arr.get(i).size())));
         }
         return outList;
+    }
+    private static <T extends Comparable<?super T>> ArrayList<T> merge(ArrayList<T> arrLeft,ArrayList<T> arrRight){
+        ArrayList<T> returnList = new ArrayList<T>();
+        int j=0;
+        int i=0;
+        while(i<arrLeft.size()&&j<arrRight.size()){
+                if(arrLeft.get(i).compareTo(arrRight.get(j))<0){
+                    returnList.add(arrLeft.get(i));
+                    i++;
+                } else if(arrLeft.get(i).compareTo(arrRight.get(j))>0) {
+                    returnList.add(arrRight.get(j));
+                    j++;
+                } else if(arrLeft.get(i).compareTo(arrRight.get(j))==0){
+                    returnList.add(arrLeft.get(i));
+                    returnList.add(arrRight.get(j));
+                    j++;
+                    i++;
+                }
+
+            }
+        while(j<arrRight.size()){
+            returnList.add(arrRight.get(j));
+            j++;
+        }
+        while(i<arrLeft.size()){
+            returnList.add(arrLeft.get(i));
+            i++;
+        }
+
+
+        return returnList;
     }
 
 }
--- src/assign05/ArrayListSorterTester.java
+++ src/assign05/ArrayListSorterTester.java
@@ -2,18 +2,19 @@ package assign05;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 public class ArrayListSorterTester {
-    ArrayList<Integer> list16 = new ArrayList();
+    ArrayList<Integer> list1024 = new ArrayList();
     @BeforeEach
     public void setUp(){
-        list16 = ArrayListSorter.generatePermuted(16);
+        list1024 = ArrayListSorter.generatePermuted(1024);
     }
     @Test
     public void mergesortTest(){
-        ArrayListSorter.mergesort(list16);
+        ArrayListSorter.mergesort(list1024);
+        assertArrayEquals(list1024.toArray(),ArrayListSorter.generateAscending(1024).toArray());
 
     }
 
