7e84d9ac228f397761c4d8d9499b88c14cdba75d
Eden Harvey <edensharvey111@gmail.com>
2023-02-16 18:31:25 -0700

    MergeSort is now a driver/passenger program  added testing for mutators and accessor methods for sizeToSwitch added functionality to switch to insertionSort

--- src/assign05/ArrayListSorter.java
+++ src/assign05/ArrayListSorter.java
@@ -15,11 +15,14 @@ public class ArrayListSorter {
         ArrayList<T> temp = new ArrayList<>(arr.size());
         for(int i=0;i<arr.size();i++)
             temp.add(null);
-        split(arr,temp,0,arr.size());
+        split(arr,temp,0,arr.size()-1);
     }
 
     public static void setSizeToSwitch(int val) {
         sizeToSwitch = val;
+    }
+    public static int getSizeToSwitch(){
+        return sizeToSwitch;
     }
 
     public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> arr) {
@@ -55,52 +58,52 @@ public class ArrayListSorter {
     }
 
     public static <T extends Comparable<? super T>> void split(ArrayList<T> arr, ArrayList<T> temp, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - 1) / 2;
+        if (left + sizeToSwitch < right) {
+            int mid = (left + (right)) / 2;
             split(arr, temp, left, mid);
-            split(arr, temp, mid + 1, right);
+            split(arr, temp, mid+1, right);
 
             merge(arr, temp, left, right);
+        }
+        if(left+sizeToSwitch>=right){
+            InsertionSort.sort(arr,left,right+1);
         }
     }
 
 
     public static <T extends Comparable<? super T>> void merge(ArrayList<T> arr, ArrayList<T> temp, int left, int right) {
-        int mid = left + (right - 1) / 2;
+        int mid = (left + (right)) / 2;
         int j = 0;
         int i = 0;
         int place = left;
 
-        while (i < mid && j < right) {
-            if (arr.get(i + left).compareTo(arr.get(j + mid)) < 0) {
+        while (i + left <= mid && j + mid +1 <= right) {
+            if (arr.get(i + left).compareTo(arr.get(j + mid +1)) < 0) {
                 temp.set(place, (arr.get(i)));
                 i++;
                 place++;
-            } else if (arr.get(i + left).compareTo(arr.get(j + mid)) > 0) {
-                temp.set(place, arr.get(j + mid));
+            } else if (arr.get(i + left).compareTo(arr.get(j + mid +1)) > 0) {
+                temp.set(place, arr.get(j + mid+1));
                 j++;
                 place++;
-            } else if (arr.get(i + left).compareTo(arr.get(j + mid)) == 0) {
+            } else if (arr.get(i + left).compareTo(arr.get(j + mid+1)) == 0) {
                 temp.set(place, arr.get(i + left));
                 i++;
-                place++;
-                temp.set(place, arr.get(j + mid));
-                j++;
                 place++;
             }
 
         }
-        while (j < right) {
-            temp.set(place, arr.get(j + mid));
+        while (j + mid < right) {
+            temp.set(place, arr.get(j + mid+1));
             j++;
             place++;
         }
-        while (i < mid) {
+        while (i + left <= mid) {
             temp.set(place, arr.get(i + left));
             i++;
             place++;
         }
-        for(int k=left;k<right;k++){
+        for(int k=left;k<=right;k++){
             arr.set(k,temp.get(k));
         }
     }
--- src/assign05/ArrayListSorterTester.java
+++ src/assign05/ArrayListSorterTester.java
@@ -2,28 +2,41 @@ package assign05;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
 
 public class ArrayListSorterTester {
     ArrayList<Integer> list1024 = new ArrayList();
     ArrayList<Integer> fourList = new ArrayList();
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
 
-        list1024 = ArrayListSorter.generatePermuted(4);
+        list1024 = ArrayListSorter.generateDescending(1024);
 
 
     }
 
+    @Test
+    public void getSwitchValueTest() {
+        assertEquals(ArrayListSorter.getSizeToSwitch(),4);
+    }
+    @Test
+    public void mutateSwitchValueTest(){
+        ArrayListSorter.setSizeToSwitch(8);
+        assertEquals(ArrayListSorter.getSizeToSwitch(),8);
+    }
 
     @Test
-    public void mergesortTest(){
+    public void mergesortTest() {
         ArrayListSorter.mergesort(list1024);
+        assertArrayEquals(list1024.toArray(), ArrayListSorter.generateAscending(1024).toArray(), "mergeSort for list 1024 works properly");
     }
 
     @Test
-    public void quicksortTest(){
+    public void quicksortTest() {
 
     }
 
