9c0f043d726823b9fec21611b0faf582eda0e52f
JonathanK3 <u1400888@utah.edu>
2023-03-23 16:24:21 -0600

    remove method frrr

--- src/assign08/BinarySearchTreeTester.java
+++ src/assign08/BinarySearchTreeTester.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class BinarySearchTreeTester {
@@ -81,8 +82,32 @@ public class BinarySearchTreeTester {
     }
     @Test
     void removeAllTest(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        expected.add(75);
+        expected.add(100);
+        expected.add(0);
+        emptyInt.addAll(expected);
+        assertFalse(emptyInt.isEmpty());
+        emptyInt.removeAll(expected);
+        System.out.println(emptyInt.toArrayList());
+        assertTrue(emptyInt.isEmpty());
 
     }
+    @Test
+    public void isEmptyAfterRemove(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        emptyInt.addAll(expected);
+        assertFalse(emptyInt.isEmpty());
+        emptyInt.remove(50);
+        emptyInt.remove(25);
+        System.out.println(emptyInt.toArrayList());
+        assertTrue(emptyInt.isEmpty());
+    }
+
     @Test
     void isEmptyTest(){
         assertTrue(emptyInt.isEmpty());
@@ -142,4 +167,101 @@ public class BinarySearchTreeTester {
 
 
     }
+    @Test
+    public void leafRemove(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        expected.add(75);
+        emptyInt.addAll(expected);
+        emptyInt.remove(25);
+        emptyInt.remove(75);
+        System.out.println(emptyInt.toArrayList());
+        assertTrue(emptyInt.contains(50));
+        assertFalse(emptyInt.contains(25));
+        assertFalse(emptyInt.contains(75));
+    }
+    @Test
+    public void oneChildRemove(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        expected.add(75);
+        expected.add(100);
+        expected.add(0);
+        emptyInt.addAll(expected);
+        emptyInt.remove(25);
+        emptyInt.remove(75);
+        System.out.println(emptyInt.toArrayList());
+        assertTrue(emptyInt.contains(50));
+        assertTrue(emptyInt.contains(0));
+        assertTrue(emptyInt.contains(100));
+        assertFalse(emptyInt.contains(25));
+        assertFalse(emptyInt.contains(75));
+    }
+
+    @Test
+    public void twoChildRemove(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        expected.add(75);
+        expected.add(100);
+        expected.add(0);
+        emptyInt.addAll(expected);
+        emptyInt.remove(50);
+        System.out.println(emptyInt.toArrayList());
+        assertFalse(emptyInt.contains(50));
+        assertTrue(emptyInt.contains(0));
+        assertTrue(emptyInt.contains(100));
+        assertTrue(emptyInt.contains(25));
+        assertTrue(emptyInt.contains(75));
+    }
+    @Test
+    void removeAllPartialTest(){
+        ArrayList expected = new ArrayList();
+        expected.add(50);
+        expected.add(25);
+        expected.add(75);
+        expected.add(100);
+        expected.add(0);
+        expected.add(35);
+        ArrayList remove = new ArrayList();
+        remove.add(50);
+        remove.add(25);
+        remove.add(0);
+        emptyInt.addAll(expected);
+        assertFalse(emptyInt.isEmpty());
+        assertTrue(emptyInt.removeAll(remove));
+        System.out.println(emptyInt.toArrayList());
+        assertTrue(emptyInt.contains(35));
+        assertTrue(emptyInt.contains(75));
+        assertTrue(emptyInt.contains(100));
+        assertFalse(emptyInt.contains(0));
+        assertFalse(emptyInt.contains(50));
+        assertFalse(emptyInt.contains(25));
+
+    }
+    @Test
+    void removeAllRandom(){
+        ArrayList original = new ArrayList<>();
+        for(int i =0; i< 20; i++)
+            original.add(i);
+        original.add(1);
+        original.add(2);
+        original.add(3);
+        original.add(4);
+        System.out.println("original "+ original.toString());
+        ArrayList remove = new ArrayList();
+        for(int i =0; i<10;i++){
+            remove.add(original.get(i));
+
+        }
+        System.out.println("remove "+ remove.toString());
+        emptyInt.addAll(original);
+        System.out.println("BST "+emptyInt.toArrayList());
+        assertTrue(emptyInt.removeAll(remove));
+        System.out.println(emptyInt.toArrayList());
+    }
+
 }
