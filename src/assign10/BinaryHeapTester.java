b7f4398fb18df7270592baa9d11d232ae03e12a3
JonathanK3 <u1400888@utah.edu>
2023-04-10 18:14:21 -0600

    Testing class

---
+++ src/assign10/BinaryHeapTester.java
@@ -0,0 +1,51 @@
+package assign10;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class BinaryHeapTester {
+    @BeforeEach
+    void setup(){
+        int[] arr = new int[8];
+    }
+    @Test
+    void insertTest(){
+
+    }
+    @Test
+    void extractMaxTest(){
+
+    }
+    @Test
+    void clearTest()
+    {
+
+    }
+    @Test
+    void sizeTest(){
+
+    }
+    @Test
+    void isEmptyTest(){
+
+    }
+    @Test
+    void buildHeapConstructor(){
+
+    }
+    @Test
+    void comparatorConstructor(){
+
+    }
+    @Test
+    void comparatorAdd(){
+
+    }
+    @Test
+    void comparatorExtractMax(){
+
+    }
+    @Test
+    void peekTest(){
+
+    }
+}
