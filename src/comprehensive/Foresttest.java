6aa103d90cc669db72f701104447920ab14c0c7f
Eden Harvey <edensharvey111@gmail.com>
2023-04-24 15:48:43 -0600

    Added shell classes and Interface

--- src/comprehensive/Foresttest.java
+++ src/comprehensive/Foresttest.java
@@ -57,9 +57,77 @@ public class Foresttest {
     }
 
     @Test
-    void testCompression(){
+    void testCompressionKeepsRep(){
+        var x = new DisjointSetForest<>();
 
-        dsf.makeSet();
+        x.makeSet(1);
+        x.makeSet(2);
+        x.makeSet(3);
+        x.makeSet(4);
+        x.makeSet(5);
+        x.makeSet(6);
+        x.makeSet(7);
+        x.makeSet(8);
+        x.makeSet(9);
+        x.makeSet(10);
+        x.makeSet(11);
+        x.makeSet(12);
+
+
+        x.union(1,2);
+        x.union(1,3);
+        x.union(1,4);
+        x.union(1,5);
+        x.union(1,6);
+        x.union(1,7);
+        x.union(1,8);
+        x.union(1,9);
+        x.union(1,10);
+        x.union(1,11);
+        x.union(1,12);
+
+        for(int i=1;i<=12;i++)
+            assertEquals(x.getRepresentative(i),1);
+
+
+
+    }
+    @Test
+    void testAllValWorksOnUnion(){
+
+
+            DisjointSetForest dsf = new DisjointSetForest();
+            dsf.makeSet(1);
+            dsf.makeSet(2);
+            dsf.makeSet(3);
+            dsf.makeSet(4);
+            dsf.union(1,4);
+            dsf.union(2,3);
+            dsf.union(2,4);
+            assertEquals(dsf.getRepresentative(1),dsf.getRepresentative(4));
+
+        dsf = new DisjointSetForest();
+        dsf.makeSet(1);
+        dsf.makeSet(2);
+        dsf.makeSet(3);
+        dsf.makeSet(4);
+        dsf.union(4,1);
+        dsf.union(3,2);
+        dsf.union(4,2);
+        assertEquals(dsf.getRepresentative(1),dsf.getRepresentative(4));
+        dsf = new DisjointSetForest();
+        dsf.makeSet(1);
+        dsf.makeSet(2);
+        dsf.makeSet(3);
+        dsf.makeSet(4);
+        dsf.union(3,1);
+        dsf.union(3,2);
+        dsf.union(4,2);
+        assertEquals(dsf.getRepresentative(1),dsf.getRepresentative(4));
+
+
+
+
     }
 
 }
